9fba144db0a8bfaaab44851ec62d34e92cb6245b
Kaitlyn <k6chan@ucsd.edu>
2023-03-12 13:26:55 -0700

    remove environment variable flag

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@ const useSubDirectory = process.env.USE_SUB_DIRECTORY === 'true'
 
 // To host on ucsd.moe and animefest.ucsd.edu:
 // assetPrefix: useSubDirectory ? '/club-next-site/' : '',
-// basePath: useSubDirectory ? '/club-next-site' : '',
+// basePath: useSubDirectory ? '/club-next-site' : ''
 
 // To host on animeclub.ucsd.edu:
-// assetPrefix: useSubDirectory ? '/~animeclub/' : '',
-// basePath: useSubDirectory ? '/~animeclub' : '',
+// assetPrefix: '/~animeclub/',
+// basePath: '/~animeclub'
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: useSubDirectory ? '/club-next-site/' : '',
-  basePath: useSubDirectory ? '/club-next-site' : '',
+  assetPrefix: '/~animeclub/',
+  basePath: '/~animeclub'
 }
